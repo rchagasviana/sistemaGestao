6d14ed94e0437776bb0659e46a4a74c530134973
Rafael Viana <rchagas.sdi@gmail.com>
2021-02-19 20:10:46 -0300

    implementanto-rotas-principal-sobreNos-contatos-v1.2

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,23 @@
 use Illuminate\Support\Facades\Route;
 
 //Aula 01 - Entendendo rodas sem redirecionar para uma view.
-
 //Rota para página inicial da aplicação
 Route::get('/', function () {
     echo "PáginaInicial";
 });
-
 //Rota para página sobre-nos
 Route::get('/sobre-nos', function() {
     echo "sobre-nos";
 });
-
 //Rota para página de contatos
 Route::get('/contatos',function(){
     echo "contatos";
 });
-
+/*
+ATENÇÃO: 
+Route:get(CAMINHO,AÇÃO);
+CAMINHO: acessado pelo usuário. Exemplo: "/usuario/cadastrar", "/paginaInicial" etc.
+AÇÃO: chamar um método e passar parâmetros para um controlador e posteriomente salvar em uma base de dados.
+Retornar uma string ou algo do tipo.
+*/
 
