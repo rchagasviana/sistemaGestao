983699d26fb16ddf06f4162acd337840a2b6aa28
Rafael Viana <rchagas.sdi@gmail.com>
2021-02-27 15:59:45 -0300

    Aula 14-Sintaxe do Blade-Didática

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 /*
-Aula 13-Encaminhando parâmetros da controlador para view
+Aula 14-Sintaxe do Blade - Didática
+
+Blade é o motor de renderização de Views do Laravel e possui uma sintaxe
+de programação PHP mais simples, além de um modelo de herança de tamplates que facilita
+o reaporveitamento do código. É importante lembrar que a sintaxe pura do PHP não é impedida
+de ser usada.
 */
 use Illuminate\Support\Facades\Route;
 /*
@@ -11,25 +16,20 @@ direcioná-la para /app/clientes podemoss chamá-la pelo seu "apelido".
 A vantagem de nomear as rotas é a indepência entre a rota e os links, umas vezes que estes
 podem ser chamados através dos nomes e não do caminho da rota
  */
+//Rotas-padrão
 Route::get('/','PrincipalController@principal')->name('site.index');
-Route::get('/sobrenos','SobreNosController@sobrenos')->name('site.sobrenos'); //pode ser escolhido qualquer nome, no entanto para efeitos de organização, usa-se o mesmo da view para qual o controlador irá direcionar
+Route::get('/sobrenos','SobreNosController@sobrenos')->name('site.sobrenos'); 
 Route::get('/contato','ContatoController@contato')->name('site.contato');
 Route::get('/login',function(){return "rotaLogin";});
 //Rotas que serão destinadas à área restrita do sistema
 Route::prefix('/app')->group(function(){//o método group() espera receber uma função de callback que receberás as rotas que farão paerte do grupo
-    Route::get('/clientes',function(){return "rotaClientes";});
-    Route::get('/fornecedores',function(){return "rotaFornecedores";});
-    Route::get('/produtos',function(){return "rotaProdutos";});
+    Route::get('/clientes',function(){return "rotaClientes";})->name('app.clientes');
+    Route::get('/fornecedores','FornecedorController@index')->name('app.fornecedores');
+    Route::get('/produtos',function(){return "rotaProdutos";})->name('app.produtos');
 });
-//Rota de fallback
+//Rota de fallback - Encaminha quando ocorre um erro na aplicação
 Route::fallback(function(){
     return '<h1>Erro 404</h1><a href="'.route('site.index').'">Clique aqui</a> para ir para Página Inicial';
 });
-//Encaminhando parâmetros para o Controlador
+//Encaminhando parâmetros para o Controlador - Rota de teste
 Route::get('/teste/{p1}/{p2}','TesteController@teste')->name('teste')->where('p1','[0-9]+')->where('p2','[0-9]+');
-
-/*
-Atenção: É importante observar na ordem de recebimento dos valores pelo controlador.
-Fica implícito que serão passados da seguinte forma TesteController@teste($p1,$p2), logo
-a ordem com que os parâmetros serão recebido no método resposável deve ser respeitada
-*/
